7f589a431d629a1712bb0495f6e1a730ed808d4a
KarlaJu <karla@makingdevs.com>
2019-07-13 17:22:05 -0500

    using v-bind for varibles in index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warn, funny socks'
+    description: 'A pair of warn, funny socks',
+    image: 'https://cdn.shopify.com/s/files/1/0558/2081/products/simpson_homealone_archive_thumb_3fb6b0e2-fc27-47c6-9107-2a12f0e1a319_1024x1024.jpeg?v=1529728220'
   }
 })
