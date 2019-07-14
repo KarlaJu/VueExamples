f18eda5b66476c2d9f2cf121052808e656466900
KarlaJu <karla@makingdevs.com>
2019-07-13 22:55:51 -0500

    using v-if to show if the product is in stock

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = new Vue({
   data: {
     product: 'Socks',
     description: 'A pair of warn, funny socks',
-    image: 'https://cdn.shopify.com/s/files/1/0558/2081/products/simpson_homealone_archive_thumb_3fb6b0e2-fc27-47c6-9107-2a12f0e1a319_1024x1024.jpeg?v=1529728220'
+    image: 'http://4.bp.blogspot.com/-7OLAdV7SZwg/T7PPDn5L-hI/AAAAAAAAAEc/VS72sPK_Z_E/s1600/ilustraciones-pinturas-surrealistas-surrealismo-dise%C3%B1o-Jacek-Yerka.jpg',
+    link: 'https://www.amazon.com/Staroar-Special-Crystal-Rhinestone-Painting/dp/B07QHTY6WS/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=paintings&qid=1563057027&s=arts-crafts-intl-ship&sr=1-1',
+    inStock: true
   }
 })
