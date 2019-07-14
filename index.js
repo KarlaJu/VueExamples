018dc99f4f7b6bf2568320421cd8c194892a216f
KarlaJu <karla@makingdevs.com>
2019-07-14 01:04:51 -0500

    using computed properties for title

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
+    brand: 'Vue Mastery',
     description: 'A pair of warn, funny socks',
     image: 'http://4.bp.blogspot.com/-7OLAdV7SZwg/T7PPDn5L-hI/AAAAAAAAAEc/VS72sPK_Z_E/s1600/ilustraciones-pinturas-surrealistas-surrealismo-dise%C3%B1o-Jacek-Yerka.jpg',
     link: 'https://www.amazon.com/Staroar-Special-Crystal-Rhinestone-Painting/dp/B07QHTY6WS/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=paintings&qid=1563057027&s=arts-crafts-intl-ship&sr=1-1',
@@ -28,6 +29,11 @@ var app = new Vue({
     },
     updateProduct(variantImage) {
       this.image = variantImage
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
     }
   }
 })
