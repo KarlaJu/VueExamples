ebd524aea1c6f742550221f8107c32b919627264
KarlaJu <karla@makingdevs.com>
2019-07-13 23:24:27 -0500

    v-show only to show and hide

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = new Vue({
     description: 'A pair of warn, funny socks',
     image: 'http://4.bp.blogspot.com/-7OLAdV7SZwg/T7PPDn5L-hI/AAAAAAAAAEc/VS72sPK_Z_E/s1600/ilustraciones-pinturas-surrealistas-surrealismo-dise%C3%B1o-Jacek-Yerka.jpg',
     link: 'https://www.amazon.com/Staroar-Special-Crystal-Rhinestone-Painting/dp/B07QHTY6WS/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=paintings&qid=1563057027&s=arts-crafts-intl-ship&sr=1-1',
-    inStock: true,
+    inStock: false,
     inventory: 100
   }
 })
