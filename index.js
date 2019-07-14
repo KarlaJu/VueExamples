049850989c39a54bdafbded7c2f9061e7bd0c24e
KarlaJu <karla@makingdevs.com>
2019-07-13 23:31:53 -0500

    v-for to display details list

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var app = new Vue({
     image: 'http://4.bp.blogspot.com/-7OLAdV7SZwg/T7PPDn5L-hI/AAAAAAAAAEc/VS72sPK_Z_E/s1600/ilustraciones-pinturas-surrealistas-surrealismo-dise%C3%B1o-Jacek-Yerka.jpg',
     link: 'https://www.amazon.com/Staroar-Special-Crystal-Rhinestone-Painting/dp/B07QHTY6WS/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=paintings&qid=1563057027&s=arts-crafts-intl-ship&sr=1-1',
     inStock: false,
-    inventory: 100
+    inventory: 100,
+    details: ["80% Cotton", "20% polyester", "Gender-natural"]
   }
 })
